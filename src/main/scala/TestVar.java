6701d07f2ca428a11012dbc4f7760cbcef1949d9
laodacsu <591021927@qq.com>
2020-01-11 14:54:34 +0800

    类、对象

--- src/main/scala/TestVar.java
+++ src/main/scala/TestVar.java
@@ -8,8 +8,14 @@ public class TestVar {
     static void cal(){
         System.out.println(a[0]);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        System.out.println(a[0]);
         cal();
+        Thread t1 = new Thread();
+        Thread t2 = new Thread();
+        t1.sleep(1000);
+        Thread.sleep(1000);  //sleep与t1无关，与Thread（当前运行线程）
+        t2.wait();
+
     }
 }
--- src/main/scala/scalaPerson.scala
+++ src/main/scala/scalaPerson.scala
@@ -4,6 +4,7 @@ object scalaPerson {
     val a:scalap2 = new scalap2()
     a.setName("laoda")
     print(a.getName())
+    println(scalap2.sex)
   }
 }
 
@@ -23,7 +24,12 @@ class scalap2{
   }
 
 }
-
+//如果希望类中存在静态内容，可以使scala中特有的伴生对象
+//伴生对象的名称应该和类名相同
+//  从技术角度讲，scala还是没有静态的内容，只不过将伴生对象又生成了一个新的类，
+object scalap2{
+  var sex:Boolean = true
+}
 class scalaP3{
   private var name : String = _
 //  默认的get，set方法为public
