a8c96662d097f820041b31310cd6c78a22a8d072
laodacsu <591021927@qq.com>
2020-01-11 17:48:12 +0800

    scala函数

--- src/main/scala/scala_grammer/lesson01.scala
+++ src/main/scala/scala_grammer/lesson01.scala
@@ -1,5 +1,22 @@
 package scala_grammer
-
+import scala.math._
 object lesson01 {
+  def main(args: Array[String]): Unit = {
+    print(sqrt(100))
+    println()
+    for(i <- 1 to 3){
+      print(i + " ")
+    }
+    println()
+    print("------------------")
+    println()
 
+    for(i <- 1 until 3) {
+      print(i + " ")
+    }
+    println()
+
+  }
+//  实际上是方法，进行了简化
+  def username = "zhangsan"
 }
